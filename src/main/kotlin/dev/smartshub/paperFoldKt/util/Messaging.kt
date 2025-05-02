5ca32a28e545873196b6d437af7039266c8a7881
Bryan Valdez <bryan501th@gmail.com>
2025-05-02 03:53:34 -0600

    Support link in getParsed

--- src/main/kotlin/dev/smartshub/paperFoldKt/util/Messaging.kt
+++ src/main/kotlin/dev/smartshub/paperFoldKt/util/Messaging.kt
@@ -4,7 +4,12 @@ import dev.smartshub.paperFoldKt.common.module.LangManager
 import dev.smartshub.paperFoldKt.localization.LanguagePack
 import dev.smartshub.paperFoldKt.module.PluginRepository
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.event.HoverEvent
+import net.kyori.adventure.text.minimessage.Context
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.Tag
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue
 import org.bukkit.entity.Player
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -24,8 +29,28 @@ object Messaging: KoinComponent {
         var text = message
         if (placeholderApiEnabled) { PlaceholderParse.parse(this, text)} //placeholders
         text = LegacyMessage.fromLegacy(text)                       //legacy
-        var component = MiniMessage.miniMessage().deserialize(text) //minimessage
+        // usage: <a:https://yourlink>TextToShow</a>
+        //TODO: maybe should use regex here
+        var component = if(text.contains("<a:")) { //minimessage
+            val extendedInstance = MiniMessage.builder()
+                .editTags{b -> b.tag("a",::createA) }
+                .build()
+            extendedInstance.deserialize(text)
+        } else {
+            MiniMessage.miniMessage().deserialize(text)
+        }
         return component                                            //return
+    }
+
+    fun createA(args: ArgumentQueue, context: Context): Tag {
+        val link = args.popOr("The <a> tag requires exactly one argument, the link to open").value()
+
+        val content = Component.text("Open $link")
+        return Tag.styling(
+            ClickEvent.openUrl(link),
+            HoverEvent.showText(content)
+        )
+
     }
 
     fun Player.getLanguagePack(): LanguagePack? = langManager.getForLang(this.locale())
